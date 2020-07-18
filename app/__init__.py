3091c79b0560dd11c35f6d1103609587ebec8584
Edward Webb <edward.webb9@outlook.com>
2020-07-18 18:22:39 +1000

    feat (.env): Added .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "C\x07\xc2\x8c,a{My\x9f\xca\xe3.\x9b*\xef\xb6\xd6\xf3\x18\x82\xee\xbb\xd3"
+app.config["SECRET_KEY"] = os.getenv("secret_key")
 # flask_app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app import routes
--- app/populartimes_api.py
+++ app/populartimes_api.py
@@ -1,8 +1,19 @@
-import livepopulartimes, json
+import livepopulartimes, json, os
+import app.database
 
 def getPopularTimes(id):
-    result = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyASUNkGuA2TVIMetjobbJTAgyz6y29b4bE", id)
-    return result
+    command = "SELECT * FROM places WHERE place_id = %s"
+    values = (id,)
+    result_db = app.database.fetch(command, values)
+    if (len(result_db) == 0):
+
+        result = livepopulartimes.get_populartimes_by_PlaceID(os.getenv("google_api_key"), id)
+        json_string = json.dumps(result)
+        command = "INSERT INTO places (place_id, popular_times_response) VALUES (%s, %s)"
+        values = (id, json_string)
+        app.database.execute(command, values,)
+        return result
+    return json.loads(result_db[0][1])
 
 
 if __name__ == '__main__':
