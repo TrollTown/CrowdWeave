7f6aad2f8271336325e1d01a375e06b4a305e443
Edward Webb <edward.webb9@outlook.com>
2020-07-18 19:16:59 +1000

    feat (health): added health db script, font file to css

--- scripts.sql
+++ scripts.sql
@@ -7,3 +7,13 @@ popular_times_response TEXT);
 CREATE TABLE ratings (
 place_id TEXT NOT NULL,
 rating INTEGER);
+
+CREATE TABLE infections (
+    notification_date TEXT,
+    postcode TEXT,
+    source_of_infection TEXT,
+    suburb_code TEXT,
+    suburb_name TEXT,
+    lga_code TEXT,
+    lga_name TEXT
+);
