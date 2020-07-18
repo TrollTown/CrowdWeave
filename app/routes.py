5fe876fba9e930e4e056336262bf1290b335686a
Edward Webb <edward.webb9@outlook.com>
2020-07-18 20:30:37 +1000

    feat (covidsafe): Added covidsafe score route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, populartimes_api
+from app import app, populartimes_api, database
 from flask import request, render_template
 
 @app.route("/", methods=['GET', 'POST'])
@@ -9,3 +9,23 @@ def index():
 def populartimes():
     place_id = request.args.get('place_id')
     return populartimes_api.getPopularTimes(place_id)
+
+@app.route("/covidsafeScore", methods=['GET'])
+def covidsafeScore():
+    place_id = request.args.get('place_id')
+    # Get number of google reviews
+    populartimes_result = populartimes_api.getPopularTimes(place_id)
+    numRatings = populartimes_result['rating_n']
+    numRatings = numRatings
+    covidSafeScore = 10.3 - ((132 + (-263)/(1 + pow((numRatings / 1592), 0.001977))) * 10)
+    postcode = populartimes_result['address'][4:]
+    command = 'SELECT * FROM infections WHERE postcode=%s'
+    values = (postcode,)
+    db_result = database.fetch(command, values)
+    if (len(db_result) != 0):
+        covidSafeScore = covidSafeScore / 2
+    
+
+    return {
+        "score": covidSafeScore
+    }
