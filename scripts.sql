912b60ff9ed56233186feb708fdb5d525ea0dddd
Edward Webb <edward.webb9@outlook.com>
2020-07-18 17:16:41 +1000

    sql script

---
+++ scripts.sql
@@ -0,0 +1,9 @@
+-- Create places table
+CREATE TABLE places (
+covidsafe(> place_id TEXT NOT NULL,
+covidsafe(> popular_times_response TEXT);
+
+-- Create ratings table
+CREATE TABLE ratings (
+covidsafe(> place_id TEXT NOT NULL,
+covidsafe(> rating INTEGER);
