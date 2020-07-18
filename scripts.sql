2a8681c845e92d263da737bef18f5c52a49e47c8
Edward Webb <edward.webb9@outlook.com>
2020-07-18 17:21:36 +1000

    fix (script): typo

--- scripts.sql
+++ scripts.sql
@@ -1,9 +1,9 @@
 -- Create places table
 CREATE TABLE places (
-covidsafe(> place_id TEXT NOT NULL,
-covidsafe(> popular_times_response TEXT);
+place_id TEXT NOT NULL,
+popular_times_response TEXT);
 
 -- Create ratings table
 CREATE TABLE ratings (
-covidsafe(> place_id TEXT NOT NULL,
-covidsafe(> rating INTEGER);
+place_id TEXT NOT NULL,
+rating INTEGER);
